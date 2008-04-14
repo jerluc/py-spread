2a1569bb37f35012b142e6633760b64d8fa4a3dd
wglin122 <wglin122@9040ea7b-954a-0410-b006-6bf8e6323ab7>
2008-04-14 07:45:37 +0000

    协议头生成函数

--- sp_class/untils.py
+++ sp_class/untils.py
@@ -8,31 +8,38 @@ Copyright (c) 2008 __MyCompanyName__. All rights reserved.
 """
 import struct
 
-from __init__ import serviceType_dic,TAG
+serviceType_dic = {
+    'JOIN_MESS'  : [0,1,0,0],
+    'LEAVE_MESS' : [0,2,0,0],
+    'KILL_MESS'  : [0,4,0,0],
+    'SEND_MESS'  : [0,0,0,2],
+}
+
+TAG = '!4B32s12B%s'
 
 
-def protocol_Create(serTpye,pname,gname):
-    
+def protocol_Create(serTpye,pname,gname,data_len=0):
     msg_header = serviceType_dic.get(serTpye)
     msg_header.append(pname)
     
     toB(msg_header, len(gname));
-    print msg_header
 
-    toB(msg_header, (0 << 8) & 0x00FFFF00);
+    toB(msg_header, 0);
     
-    #buffer = [0,0,0,2,pname, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, len(pname), gname]
-    #p = struct.pack(TAG,*buffer)
-    #print p
+    toB(msg_header, data_len);
+    
+    for g in gname:
+        msg_header.append(g)
+
+    tag = TAG%('32s'*len(gname))
+    p = struct.pack(tag,*msg_header)
+    return  p
 
 def toB(buffer,i):
-	buffer.append('1')
-
-def toBName(buffer,bufferIndex,name):
-	for i in name:
- 		buffer[bufferIndex] = i
- 		bufferIndex = bufferIndex + 1
+    for x in [24,16,8,0]:
+		  buffer.append((i >> x) & 0xFF)
 
 if __name__ == '__main__':
-	protocol_Create('JOIN_MESS','#test#machine1',['spreadtest',])
+	p = protocol_Create('JOIN_MESS','#test#machine1',['spreadtest',])
+	print p
 
